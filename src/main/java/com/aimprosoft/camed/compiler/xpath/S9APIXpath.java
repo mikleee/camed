28f0ba6636fa1dbed59d2b99809858a33caef5f5
mikleee <mikle.tkachenko@gmail.com>
2015-02-26 18:00:07 +0200

    home

--- src/main/java/com/aimprosoft/camed/compiler/xpath/S9APIXpath.java
+++ src/main/java/com/aimprosoft/camed/compiler/xpath/S9APIXpath.java
@@ -1,10 +1,7 @@
 package com.aimprosoft.camed.compiler.xpath;
 
 import net.sf.saxon.jdom.DocumentWrapper;
-import net.sf.saxon.om.NodeInfo;
-import net.sf.saxon.om.VirtualNode;
 import net.sf.saxon.s9api.*;
-import org.jdom.Content;
 import org.jdom.Document;
 import org.jdom.Namespace;
 
@@ -25,34 +22,6 @@ public class S9APIXpath {
         db = proc.newDocumentBuilder();
         xdmDoc = db.wrap(saxonDoc);
         xpath = proc.newXPathCompiler();
-    }
-
-
-    public List<Content> selectNodes(String expr) throws SaxonApiException {
-        XPathExecutable xx = xpath.compile(expr);
-        XPathSelector selector = xx.load();
-        selector.setContextItem(xdmDoc);
-        List<Content> content = new ArrayList<Content>();
-        for (XdmItem item : selector) {
-            NodeInfo saxonNode = ((XdmNode) item).getUnderlyingNode();
-            content.add((Content) ((VirtualNode) saxonNode).getUnderlyingNode());
-        }
-        return content;
-    }
-
-    public Content selectNode(String expr) throws SaxonApiException {
-        XPathExecutable xx = xpath.compile(expr);
-        XPathSelector selector = xx.load();
-        selector.setContextItem(xdmDoc);
-        List<Content> content = new ArrayList<Content>();
-        for (XdmItem item : selector) {
-            NodeInfo saxonNode = ((XdmNode) item).getUnderlyingNode();
-            content.add((Content) ((VirtualNode) saxonNode).getUnderlyingNode());
-        }
-        if (content.size() > 1)
-            return null;
-        else
-            return content.get(0);
     }
 
     public List<Namespace> getDocumentNamespaces() throws SaxonApiException {

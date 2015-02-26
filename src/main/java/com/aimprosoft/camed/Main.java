efd5c9674cb6a11f23ffe493ce5b559cc8d5be1b
mikleee <mikle.tkachenko@gmail.com>
2015-02-26 11:01:17 +0200

    home

--- src/main/java/com/aimprosoft/camed/Main.java
+++ src/main/java/com/aimprosoft/camed/Main.java
@@ -1,21 +1,21 @@
 package com.aimprosoft.camed;
 
-import com.aimprosoft.camed.compiler.CamCompiler;
-
 import java.io.File;
 
 
 public class Main {
 
     private static String root = "C:\\Users\\Мишаня\\IdeaProjects\\camed\\";
+    private static File input = new File(root + "resorces\\input\\UDB-cam.cam");
 
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
 
-        File input = new File(root + "resorces\\input\\UDB-cam.cam");
-        new CamCompiler(root + "resorces\\output", root + "resorces\\input\\result.cxx").compile(input);
 
+//      new CamCompiler(root + "resorces\\output", root + "resorces\\input\\result.cxx").compile(input);
+        TestCase.test(root, input);
         System.out.println("total time: " + (System.currentTimeMillis() - start));
     }
+
 
 }
---
+++ src/main/java/com/aimprosoft/camed/TestCase.java
@@ -0,0 +1,56 @@
+package com.aimprosoft.camed;
+
+import com.aimprosoft.camed.compiler.model.Constraint;
+import com.aimprosoft.camed.compiler.model.impl.CAMTemplate;
+import com.aimprosoft.camed.compiler.service.ModelFactory;
+import com.aimprosoft.camed.compiler.util.DocumentFactory;
+import org.jdom.Document;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ * @author mtkachenko.
+ */
+public class TestCase {
+
+
+    public static void test(String root, File input) throws Exception {
+        Document doc = new DocumentFactory().createDocument(input);
+        CAMTemplate result = ModelFactory.createCAMTemplate(doc);
+
+        String compiledTemplate = compilesTemplate(root, input);
+
+        int i = 0;
+        for (List<Constraint> group : result.getConstraintManager().getGroupedConstraints().values()) {
+            for (Constraint constraint : group) {
+                String condition = constraint.getCondition();
+                if (condition == null) {
+                    continue;
+                }
+                condition = condition.replaceAll("'", "&apos;").replaceAll(">", "&gt;").replaceAll("<", "&lt;");
+                if (!compiledTemplate.contains(condition)) {
+                    System.out.println("Condition : " + condition + " was not added");
+                    i++;
+                }
+            }
+        }
+
+        System.out.println("total count " + i);
+    }
+
+    private static String compilesTemplate(String root, File input) throws IOException {
+        File compiled = new File(root + "resorces\\output\\temp_cxf_output_.xml");
+        Reader reader = new BufferedReader(new FileReader(compiled));
+
+        StringBuilder compiledString = new StringBuilder();
+
+        int c = 0;
+        while ((c = reader.read()) != -1) {
+            compiledString.append((char) c);
+        }
+
+        return compiledString.toString();
+    }
+
+}
